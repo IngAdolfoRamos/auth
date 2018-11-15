fc12eb05a99faa444eb8daefc42591f11f072a20
IngAdolfoRamos <luadramo@gmail.com>
2018-11-14 22:00:15 -0600

    Creating my own login controller functionality using Auth facade

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,37 +3,22 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function login()
     {
-        $this->middleware('guest')->except('logout');
+        $credentials = $this->validate(request(),[
+            'email' => 'email|required|string',
+            'password' => 'required|string'
+        ]);
+
+        if(Auth::attempt($credentials))
+        {
+            return "Si";
+        }
+        return back()->withErrors(['email' => 'Verifica tu usuario y contraseña'])
+            ->withInput(request(['email']));
     }
 }
