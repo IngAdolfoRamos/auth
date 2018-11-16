4e21376797797208cd113fde946c794658493c62
IngAdolfoRamos <luadramo@gmail.com>
2018-11-16 09:57:32 -0600

    Adding digital credential form.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,18 +2,109 @@
 
 @section('content')
     <div class="row">
-        <div class="col-4 offset-4">
+        <div class="col-6 offset-3">
             <div class="card border-primary">
+
                 <div class="card-header">
-                    Bienvenido {{ auth()->user()->name }}
+                    <div class="h3 text-center text-capitalize">Credencial digital</div>
                 </div>
+
                 <div class="card-body">
-                    Email: {{ auth()->user()->email }}
+
+                    <form>
+                        <div class="form-row">
+
+                            <div class="form-group col-5"
+                                 style="margin-top: 1%;">
+                                <img class="" src="{{asset('/img/larm1.jpg')}}"
+                                    style="height: 85%;
+                                    width: 97%;
+                                    border-radius: 15%;">
+                            </div>
+
+                            <div class="form-group col-7">
+                                <label for="" class="lead">Nombre:</label>
+                                <div type="email" class="form-control lead text-center">
+                                    {{ auth()->user()->name }}
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="form-row" style="margin-top: -130px;">
+
+                            <div class="form-group col-4 offset-5">
+                                <label for="" class="lead">Matricula:</label>
+                                <div class="form-control lead text-center">
+                                    201514076
+                                </div>
+                            </div>
+
+                            <div class="form-group col-3">
+                                <label for="" class="lead">Promedio:</label>
+                                <div class="form-control lead text-center">
+                                    9.5
+                                </div>
+                            </div>
+
+                        </div>
+
+                        {{--<div class="form-group">
+                            <label for="inputAddress">Address</label>
+                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                        </div>--}}
+
+                        <div class="form-row">
+
+                            <div class="form-group col-5">
+                                <label for="" class="lead">Horario:</label>
+                                <div type="email" class="form-control lead text-center">
+                                    Lun - Vie | 2 pm - 8 pm
+                                </div>
+                            </div>
+
+                            <div class="form-group col-2">
+                                <label for="" class="lead">Grupo:</label>
+                                <div type="email" class="form-control lead text-center">
+                                    {{ 4851 }}
+                                </div>
+                            </div>
+
+                            <div class="form-group col-2">
+                                <label for="" class="lead">Grado:</label>
+                                <div type="email" class="form-control lead text-center">
+                                    8°
+                                </div>
+                            </div>
+
+                            <div class="form-group col-3">
+                                <label for="" class="lead">Carrera:</label>
+                                <div type="email" class="form-control lead text-center">
+                                    ISC
+                                </div>
+                            </div>
+
+                            <div class="form-group col-5">
+                                <label for="" class="lead">Tutor:</label>
+                                <div class="form-control lead text-center">
+                                    Virginia Aguilar Guerrero
+                                </div>
+                            </div>
+
+                            <div class="form-group col-7">
+                                <label for="" class="lead">Email:</label>
+                                <div class="form-control lead text-center">
+                                    {{ auth()->user()->email }}
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
                 </div>
                 <div class="card-footer">
                     <form action="{{ route('logout') }}" method="post">
                         {{ csrf_field() }}
-                        <button class="btn btn-danger btn-sm btn-block">Cerrar sesion</button>
+                        <button class="btn btn-danger btn-block">Cerrar sesion</button>
                     </form>
                 </div>
             </div>
