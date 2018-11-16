dacb371258c81505766f4f9c3c5c7b3cbe8fd691
IngAdolfoRamos <luadramo@gmail.com>
2018-11-16 09:56:54 -0600

    Adding  messages for session.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
 <body>
     <div class="container">
         <hr>
+        @if(session()->has('flash'))
+            <div class="alert alert-danger lead">{{ session('flash') }}</div>
+        @endif
         @yield('content')
     </div>
 </body>
